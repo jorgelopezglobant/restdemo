c17f19ed6fb7470474fcd652a9f8a44dacf74a5d
Jorge Lopez <jorge.lopez@globant.com>
2017-04-07 16:22:04 -0300

    TEST

--- src/main/java/com/restexample/model/CustomerRepository.java
+++ src/main/java/com/restexample/model/CustomerRepository.java
@@ -4,7 +4,7 @@ import com.restexample.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
- * Created by jorge.lopez on 4/4/2017.
+ * Created by jorge.lopez on 4/4/2017. 
  */
 public interface CustomerRepository extends JpaRepository <Customer, Integer> {
 }
